3eebd49a642bbdf602e41d333594fc6ee9c17b79
JuanCarlos <juancarlos318504@gmail.com>
2020-08-14 19:10:20 -0500

    Updates

---
+++ precache-manifest.6f846a1c5ba5c51a90bb433c55cb3914.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebf58523afeb5aa96f2fbaa73d94fdb5",
+    "url": "/VR-Question-School/index.html"
+  },
+  {
+    "revision": "df2479186313f62f2097",
+    "url": "/VR-Question-School/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "5a5a57460d21498950a9",
+    "url": "/VR-Question-School/static/js/2.93133dd0.chunk.js"
+  },
+  {
+    "revision": "3717cc2476b9ea74c496b153ef91b857",
+    "url": "/VR-Question-School/static/js/2.93133dd0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df2479186313f62f2097",
+    "url": "/VR-Question-School/static/js/main.667c4431.chunk.js"
+  },
+  {
+    "revision": "e3678cb5904d1d5fc2a1",
+    "url": "/VR-Question-School/static/js/runtime-main.45861ded.js"
+  }
+]);
